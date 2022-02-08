bb67159de102eae86561f02477bf2b80e9157929
Naitik Jindal <91480898+naitik1001@users.noreply.github.com>
2022-02-08 17:46:52 +0530

    Add files via upload

---
+++ swappingFiles.py
@@ -0,0 +1,12 @@
+def swipeFileData():
+    file1 = input("enter the path of file 1")
+    file2 = input("enter the path of file 2")
+    with open(file1,'r') as a:
+     data_a = a.read()
+    with open(file2,'r') as b:
+      data_b = b.read()
+    with open(file1,'w') as a:
+      a.write(data_b)
+    with open(file2,'w') as b:
+        b.write(data_a)
+swipeFileData()
